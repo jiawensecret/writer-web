009f355b7fce37582db2f2f38ebe9e95c3509575
Gone with the wind <294128448@qq.com>
2023-03-10 16:17:12 +0800

    feat: 初始化

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+// 运行时配置
+import { RuntimeAntdConfig } from 'umi';
+import { theme } from 'antd';
+
+export const antd: RuntimeAntdConfig = (memo) => {
+  memo.theme ||= {};
+  memo.theme.algorithm = theme.darkAlgorithm;
+  return memo;
+};
+// 全局初始化数据配置，用于 Layout 用户信息和权限初始化
+// 更多信息见文档：https://umijs.org/docs/api/runtime-config#getinitialstate
+export async function getInitialState(): Promise<{ name: string }> {
+  return { name: '@umijs/max' };
+}
+
+export const layout = () => {
+  return {
+    menu: {
+      locale: false,
+    },
+  };
+};
